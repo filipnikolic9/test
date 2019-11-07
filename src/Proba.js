7341ea9b9c02d398d27aff5ce7e9b0667fa8e04b
Filip Nikolic <filip.nikolic@heliant.rs>
2019-11-07 11:45:20 +0100

    1

--- src/Proba.js
+++ src/Proba.js
@@ -1,1 +1,8 @@
-export default retard="Vido";
+const MessageTypeProba = {
+    success: "success",
+    info: "info",
+    warning: "warning",
+    error: "danger"
+}
+
+export default MessageTypeProba;
